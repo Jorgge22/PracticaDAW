81fd2a16bacf435233bb5a86e22621c93e4e702c
Jorge <tu-email@example.com>
2026-02-05 09:30:23 +0100

    Migraciones de bicileta lista

--- database/migrations/2026_02_05_080348_create_tipo_componente_table.php
+++ database/migrations/2026_02_05_080348_create_tipo_componente_table.php
@@ -13,7 +13,8 @@ return new class extends Migration
     {
         Schema::create('tipo_componente', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->string('nombre', '50');
+            $table->string('descripcion', '255');
         });
     }
 
--- database/migrations/2026_02_05_080353_create_bicicleta_table.php
+++ database/migrations/2026_02_05_080353_create_bicicleta_table.php
@@ -13,7 +13,9 @@ return new class extends Migration
     {
         Schema::create('bicicleta', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->string('nombre', '50');
+            $table->enum('tipo', ['carretera', 'mtb', 'gravel', 'rodillo']);
+            $table->string('comentario', '255');
         });
     }
 
